116968eae8192d1f54d2428071c6051de8e3f49c
tafadzwanhesvure <nhesvuretafadzwa@gmail.com>
2024-12-10 07:49:40 +0200

    final game with interface open console to see what is happeing. game completed with interface but has not been styled.

--- javascript.js
+++ javascript.js
@@ -9,6 +9,7 @@ function handleClick (event) {
     else if (buttonId === "scissorsButton"){
         humanChoice = "scissors";
     }
+    
     const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     let computerChoice = choices[randomIndex];
@@ -24,15 +25,27 @@ function handleClick (event) {
             {
                 humanScore++;
                 resultMsg = `You win! ${humanChoice} beat ${computerChoice}`;
+                humanScoreTag.textContent = `Human Score: ${humanScore}`;
             }
         else{
                 computerScore++;
                 resultMsg = `You lose! ${computerChoice} beat ${humanChoice}`;
+                compScoreTag.textContent = `Computer Score: ${computerScore}`;
             }
+        if (humanScore >= 5){
+            humanScoreTag.textContent = `HUMAN WINS!! RESTART GAME`;
+            compScoreTag.textContent = ``;
+        }
+        else if (computerScore >= 5){
+            compScoreTag.textContent = `COMP WINS!! restart game`;
+            humanScoreTag.textContent = ``;
+        }
         return resultMsg;
         
- }
+    } 
+
 console.log(playRound(humanChoice, computerChoice));
+console.log(`human score~${humanScore} and comp score~${computerScore}`);
 }
 
 let humanScore = 0;
@@ -43,10 +56,10 @@ let gameMsg;
 const rockButton = document.getElementById('rockButton');
 const paperButton = document.getElementById('paperButton');
 const scissorsButton = document.getElementById('scissorsButton');
+const compScoreTag = document.getElementById('compScoreTag');
+const humanScoreTag = document.getElementById('humanScoreTag');
+    
 
 rockButton.addEventListener('click', handleClick);
 paperButton.addEventListener('click', handleClick);
 scissorsButton.addEventListener('click', handleClick);
-
-
-        
